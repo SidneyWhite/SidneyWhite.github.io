4ea7aaf0d36baba8b5277ed40d35a17e44389a58
Erhmee <erhembayar.ch@gmail.com>
2020-03-06 14:07:05 -0600

    lab push

--- bug/buggy1.js
+++ bug/buggy1.js
@@ -1,3 +1,13 @@
 function gogogo() {
-    alert("Yay, it works!");
+    // alert("Yay, it works!");
+    setTimeout(booyah, 2000);
+    // setTimeout(booyah(), 2000);
+}
+
+function booyah(){
+    console.log("booyah 1");
+}
+
+function booyah(){
+    console.log("booyah 1");
 }
