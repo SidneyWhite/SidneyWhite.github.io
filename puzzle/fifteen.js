e6f85d72b8fb90c3e6a649a7b1dab274f9d317ea
Erhmee <erhembayar.ch@gmail.com>
2020-03-13 11:14:47 -0500

    push

--- puzzle/fifteen.js
+++ puzzle/fifteen.js
@@ -26,10 +26,10 @@ var is_movable = () => {
         case id_3:
         case id_4:
             return true;
-            break;
+            // break;
         default:
             return false;
-            break;
+            // break;
     }
 };
 
@@ -58,20 +58,16 @@ var init = function () {
     // initialize each piece
     for (var i = 0; i < divs.length; i++) {
         var div = divs[i];
-
         div.id = "square_" + (i % 4) + "_" + (Math.floor(i / 4));
-
         // calculate x and y for this piece
         var x = ((i % 4) * 100);
         var y = (Math.floor(i / 4) * 100);
-
         // set basic style and background
         div.className = "puzzlepiece";
         div.style.left = x + 'px';
         div.style.top = y + 'px';
         div.style.backgroundImage = 'url("background.jpg")';
         div.style.backgroundPosition = -x + 'px ' + (-y) + 'px';
-
         // store x and y for later
         div.x = x;
         div.y = y;
